84834d3fb2b36c7f2038e596dd74da324776d098
AlexLatt <aleksandlatypov@gmail.com>
2023-07-03 02:50:27 +0300

    Add validation and mask AND FIX index.html part!!

--- src/js/script.js
+++ src/js/script.js
@@ -56,4 +56,41 @@ $(document).ready(function(){
 
 
     });
+
+    //validation
+
+   
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name:{
+                    required: true,
+                    minlength: 2
+                  },
+                phone:"required",
+                email:{
+                    required: true,
+                    email:true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символa!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свой email",
+                  email: "Неправельно введен email"
+                }
+              }
+        });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    //mask form
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
   });
